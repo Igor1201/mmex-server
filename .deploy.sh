97ed6705a5421c27d12077d13037b60d3f3fd719
Igor Borges <igor@borges.me>
2017-07-25 02:20:10 -0300

    make deploy work for real

--- .deploy.sh
+++ .deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -euo pipefail
 SHA=`git log -1 --abbrev=7 --format="%h"`
 DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 NAME="mmex-server"
-ENV_VARS="-e PORT=${PORT} -e DROPBOX_ACCESS_KEY='${DROPBOX_ACCESS_KEY}' -e DROPBOX_FILE_PATH='${DROPBOX_FILE_PATH}'"
+ENV_VARS="-e PORT=${PORT} -e DROPBOX_ACCESS_KEY=\"${DROPBOX_ACCESS_KEY}\" -e DROPBOX_FILE_PATH=\"${DROPBOX_FILE_PATH}\""
 
-echo "Building artifact ${SHA} on ${DATE}..."
+echo "Building artifact ${SHA} at ${DATE}..."
 tar cf ${NAME}.tar package.json Dockerfile *.js
 
 echo "Uploading artifact..."
@@ -17,7 +17,8 @@ cd ~/${NAME}
 tar xf ${NAME}.tar
 docker build -t ${NAME} .
 docker stop ${NAME}
-docker run --rm --name ${NAME} -v '${PWD}':/usr/src/app -w /usr/src/app -p ${PORT}:${PORT} -e SHA=${SHA} -e DATE='${DATE}' ${ENV_VARS} -d ${NAME}
-'" > /dev/null
+docker rm ${NAME}
+docker run --name ${NAME} -v \"\${PWD}\":/usr/src/app -w /usr/src/app -p ${PORT}:${PORT} -e SHA=${SHA} -e DATE=\"${DATE}\" ${ENV_VARS} -d ${NAME}
+'"
 
 echo "Done."
