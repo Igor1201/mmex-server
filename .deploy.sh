eca01b3937c27bcde3de410d76f8e877523bb8ee
Igor Borges <igor@borges.me>
2017-07-25 04:01:35 -0300

    export uUSER and PASS env vars

--- .deploy.sh
+++ .deploy.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 SHA=`git log -1 --abbrev=7 --format="%h"`
 DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 NAME="mmex-server"
-ENV_VARS="-e PORT=${PORT} -e DROPBOX_ACCESS_KEY=\"${DROPBOX_ACCESS_KEY}\" -e DROPBOX_FILE_PATH=\"${DROPBOX_FILE_PATH}\""
+ENV_VARS="-e PORT=${PORT} -e DROPBOX_ACCESS_KEY=\"${DROPBOX_ACCESS_KEY}\" -e DROPBOX_FILE_PATH=\"${DROPBOX_FILE_PATH}\" -e USER=${USER} -e PASSWORD=\"${PASSWORD}\""
 
 echo "Building artifact ${SHA} at ${DATE}..."
 tar cf ${NAME}.tar package.json Dockerfile *.js
